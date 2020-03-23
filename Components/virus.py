7f1bd439b382ece7277c05a02be8329823c5097b
xiaosonggege <1243049371@qq.com>
2020-03-23 22:33:21 +0800

    commit

---
+++ Components/virus.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: songyunlong
+@license: (C) Copyright 2020-2025.
+@contact: 1243049371@qq.com
+@software: pycharm
+@file: virus
+@time: 2020/3/23 10:31 下午
+'''
+import pygame
+
+class Virus:
+    pass
+
+
+if __name__ == '__main__':
+    pass
